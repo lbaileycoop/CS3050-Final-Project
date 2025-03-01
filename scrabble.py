8990088cfc1cc22a1382ca2452dd883756eb8634
izzykriz <isabellekriz1@gmail.com>
2025-03-01 14:59:13 -0500

    fixed some pylint errors

--- scrabble.py
+++ scrabble.py
@@ -1,3 +1,4 @@
+"""Module to run a Scrabble game using PyArcade"""
 import arcade
 
 WINDOW_WIDTH = 950
@@ -14,9 +15,10 @@ BORDER = 125
 BASE_TILE = './assets/blank.png'
 
 class ScrabbleUI(arcade.View):
-    def __init__(self):
-        super().__init__();
+    """Class to display and run the backend of a scrabble game"""
 
+    def __init__(self):
+        super().__init__()
 
         self.background_color = arcade.color.BISTRE_BROWN
 
@@ -28,18 +30,16 @@ class ScrabbleUI(arcade.View):
             tile.sprite.center_x = coords[0] * (TILE_SIZE + TILE_GAP) + BORDER
             tile.sprite.center_y = coords[1] * (TILE_SIZE + TILE_GAP) + BORDER
 
-            tile.textSprite.center_x = tile.sprite.center_x
-            tile.textSprite.center_y = tile.sprite.center_y
+            tile.text_sprite.center_x = tile.sprite.center_x
+            tile.text_sprite.center_y = tile.sprite.center_y
 
             self.board.append(tile.sprite)
-            self.board.append(tile.textSprite)
-        
+            self.board.append(tile.text_sprite)
 
     def reset(self):
         """Reset the game to the initial state."""
         # Do changes needed to restart the game here if you want to support that
         pass
-
 
     def on_draw(self):
         """
@@ -53,18 +53,17 @@ class ScrabbleUI(arcade.View):
 
         # Call draw() on all your sprite lists below
 
-        
         self.board.initialize()
         self.board.draw()
 
 
-    def to_coords(self, index: int) -> (int, int):
+    def to_coords(self, index: int) -> tuple[int, int]:
         """Returns the x and y values of a tile based on the 1-d index"""
         return (index % COLS, (index // COLS))
 
-    def from_coords(self, x: int, y: int) -> (int, int):
+    def from_coords(self, coords: tuple[int, int]) -> int:
         """Returns the 1-d index of a tile based on the x and y values"""
-        return x + ((ROWS - y) * COLS)
+        return coords[0] + ((ROWS - coords[1]) * COLS)
 
     def on_update(self, delta_time):
         """
@@ -75,7 +74,7 @@ class ScrabbleUI(arcade.View):
 
         pass
 
-    def on_key_press(self, key, key_modifiers):
+    def on_key_press(self, symbol, modifiers):
         """
         Called whenever a key on the keyboard is pressed.
         For a full list of keys, see:
@@ -91,7 +90,7 @@ class ScrabbleUI(arcade.View):
 
         pass
 
-    def on_mouse_motion(self, x, y, delta_x, delta_y):
+    def on_mouse_motion(self, x, y, dx, dy):
         """
         Called whenever the mouse moves.
         """
@@ -99,14 +98,14 @@ class ScrabbleUI(arcade.View):
         pass
 
 
-    def on_mouse_press(self, x, y, button, key_modifiers):
+    def on_mouse_press(self, x, y, button, modifiers):
         """
         Called when the user presses a mouse button.
         """
 
         pass
 
-    def on_mouse_release(self, x, y, button, key_modifiers):
+    def on_mouse_release(self, x, y, button, modifiers):
         """
         Called when a user releases a mouse button.
         """
@@ -114,9 +113,11 @@ class ScrabbleUI(arcade.View):
         pass
 
 class Tile():
+    """Class that represents a Scrabble tile"""
+
     def __init__(self, letter: str, value: int, is_empty: bool = False):
         self.sprite = arcade.Sprite(BASE_TILE)
-        self.textSprite = arcade.create_text_sprite(letter, arcade.color.BLACK, font_size=24)
+        self.text_sprite = arcade.create_text_sprite(letter, arcade.color.BLACK, font_size=24)
         self.sprite.size = (TILE_SIZE, TILE_SIZE)
 
         if not is_empty:
@@ -125,14 +126,10 @@ class Tile():
             self.sprite.color = arcade.color.TAN
         else:
             self.sprite.color = arcade.color.DARK_BROWN
-    
-    def update():
-        arcade.draw
 
-
-
-    
 def main():
+    """Main function to initialize and run ScrabbleUI game"""
+
     window = arcade.Window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
 
     scrabble = ScrabbleUI()
@@ -140,6 +137,5 @@ def main():
     window.show_view(scrabble)
 
     arcade.run()
-
 
 main()
