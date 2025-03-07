3fe9ad0629410b75cf3f45552693a13d957034de
Matthew Lucia <luciamatthewt@gmail.com>
2025-03-07 15:33:18 -0500

    Upload modules

---
+++ modules/__init__.py
@@ -0,0 +1,7 @@
+from .config import *
+from .tile import Tile, TILES
+from .drawbag import Drawbag
+from .rack import Rack
+from .player import Player
+from .board import Board
+from .scrabble_ui import ScrabbleUI
---
+++ modules/config.py
@@ -0,0 +1,16 @@
+import arcade
+
+WINDOW_WIDTH, WINDOW_HEIGHT = arcade.get_display_size()
+WINDOW_TITLE = "Scrabble"
+BOARD_SIZE = min(WINDOW_WIDTH, WINDOW_HEIGHT) * 0.6
+ROWS = 15
+COLS = 15
+TILE_SIZE = int(BOARD_SIZE / 15)
+TILE_GAP = TILE_SIZE * 0.1
+DOCK_SIZE_X = BOARD_SIZE
+DOCK_SIZE_Y = int(BOARD_SIZE)
+BORDER_X = (WINDOW_WIDTH - BOARD_SIZE) // 2
+BORDER_Y = (WINDOW_HEIGHT - BOARD_SIZE) // 2
+
+with open("./assets/dictionary.csv", "r") as file:
+    DICTIONARY = set([line.strip() for line in file])
